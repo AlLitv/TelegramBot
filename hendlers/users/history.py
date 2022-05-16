03db6c9d0c7f48a480898546ff5e2aae0fcaa43a
Алексей Литвинов <lyosh.2010@mail.ru>
2022-05-16 17:49:41 +1000

    "date change"

--- hendlers/users/history.py
+++ hendlers/users/history.py
@@ -15,11 +15,12 @@ async def history(user_id):
     day_three = False
     day_four = False
     day_five = False
+    date_start = datetime.today().date()
     while True:
-        date_registr = await sqllite3_bd.get_date_registr(user_id)
-        date_reg = datetime.strptime(str(date_registr), "%Y-%m-%d").date()
+        #date_registr = await sqllite3_bd.get_date_registr(user_id)
+        #date_reg = datetime.strptime(str(date_registr), "%Y-%m-%d").date()
         date_now = datetime.today().date()
-        difference = date_now - date_reg
+        difference = date_now - date_start
         time_now = datetime.now().hour
         time_start = 12
         delay_in_second = 15 * 60
@@ -32,36 +33,39 @@ async def history(user_id):
             break
         elif day_four and difference.days == 3 and time_now == time_start:
             day_namber = 4
-            await dp.bot.send_sticker(chat_id=user_id, sticker=hist.stick[day_namber])
-            await dp.bot.send_message(chat_id=user_id, text=hist.message[day_namber])
-            await dp.bot.send_photo(chat_id=user_id, photo=hist.file[day_namber])
-            await dp.bot.send_video(chat_id=user_id, video=hist.video[day_namber])
             day_four = False
             day_five = True
+            await dp.bot.send_sticker(chat_id=user_id, sticker=hist.stick[day_namber])
+            await dp.bot.send_message(chat_id=user_id, text=hist.message[day_namber])
+            await dp.bot.send_photo(chat_id=user_id, photo=hist.file[day_namber])
+            await dp.bot.send_video(chat_id=user_id, video=hist.video[day_namber])
+
         elif day_three and difference.days == 2 and time_now == time_start:
             day_namber = 3
-            await dp.bot.send_sticker(chat_id=user_id, sticker=hist.stick[day_namber])
-            await dp.bot.send_message(chat_id=user_id, text=hist.message[day_namber])
-            await dp.bot.send_photo(chat_id=user_id, photo=hist.file[day_namber])
-            await dp.bot.send_video(chat_id=user_id, video=hist.video[day_namber])
             day_three = False
             day_four = True
+            await dp.bot.send_sticker(chat_id=user_id, sticker=hist.stick[day_namber])
+            await dp.bot.send_message(chat_id=user_id, text=hist.message[day_namber])
+            await dp.bot.send_photo(chat_id=user_id, photo=hist.file[day_namber])
+            await dp.bot.send_video(chat_id=user_id, video=hist.video[day_namber])
+
         elif day_two and difference.days == 1 and time_now == time_start:
             day_namber = 2
-            await dp.bot.send_sticker(chat_id=user_id, sticker=hist.stick[day_namber])
-            await dp.bot.send_message(chat_id=user_id, text=hist.message[day_namber])
-            await dp.bot.send_photo(chat_id=user_id, photo=hist.file[day_namber])
-            await dp.bot.send_video(chat_id=user_id, video=hist.video[day_namber])
             day_two = False
             day_three = True
-        elif day_one:
-            day_namber = 1
             await dp.bot.send_sticker(chat_id=user_id, sticker=hist.stick[day_namber])
             await dp.bot.send_message(chat_id=user_id, text=hist.message[day_namber])
             await dp.bot.send_photo(chat_id=user_id, photo=hist.file[day_namber])
             await dp.bot.send_video(chat_id=user_id, video=hist.video[day_namber])
+
+        elif day_one:
+            day_namber = 1
             day_one = False
             day_two = True
+            await dp.bot.send_sticker(chat_id=user_id, sticker=hist.stick[day_namber])
+            await dp.bot.send_message(chat_id=user_id, text=hist.message[day_namber])
+            await dp.bot.send_photo(chat_id=user_id, photo=hist.file[day_namber])
+            await dp.bot.send_video(chat_id=user_id, video=hist.video[day_namber])
         await asyncio.sleep(delay_in_second)
 
 
